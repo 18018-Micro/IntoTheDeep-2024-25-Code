0b282b24f819adf296b8f0c9c1508623e8c87f4a
jejs <jejs2099@outlook.com>
2024-12-21 11:06:05 -0600

    Added new test program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testOpMode.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class testOpMode extends LinearOpMode {
+
+
+    @Override
+    public void runOpMode() {
+
+    }
+}
