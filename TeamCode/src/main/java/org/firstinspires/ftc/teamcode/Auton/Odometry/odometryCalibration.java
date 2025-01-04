9719b470cde2bc1d60e6e38accf901de40c45130
jejs2 <jejs2099@outlook.com>
2025-01-04 11:49:37 -0600

    Added GOBILDA example files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/Odometry/odometryCalibration.java
+++
@@ -1,4 +0,0 @@
-package org.firstinspires.ftc.teamcode.Auton.Odometry;
-
-public class odometryCalibration {
-}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/Vision/objectDetection.java
+++
@@ -1,7 +0,0 @@
-package org.firstinspires.ftc.teamcode.Auton.Vision;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-
-@Autonomous(name = "objectDetection", group = "")
-public class objectDetection {
-}
